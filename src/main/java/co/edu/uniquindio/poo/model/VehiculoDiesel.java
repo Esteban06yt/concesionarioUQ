2023da6e30a8ca16a100d6202f9e492d4fabc83b
Juanda2312 <juand.tapierom@uqvirtual.edu.co>
2024-11-16 18:59:11 -0500

    ejemplo de como debe ser la clase padre

--- src/main/java/co/edu/uniquindio/poo/model/VehiculoDiesel.java
+++ src/main/java/co/edu/uniquindio/poo/model/VehiculoDiesel.java
@@ -1,19 +1,16 @@
 package co.edu.uniquindio.poo.model;
 
 public abstract class VehiculoDiesel extends Vehiculo {
-    private String combustible = "Diesel";
+    private String combustible;
 
     public VehiculoDiesel(String idVehiculo, String marca, String modelo, Integer cambios, Float velocidadMaxima,
-            Float cilindraje, Transmision transmision, Estado estado, String combustible) {
+            Float cilindraje, Transmision transmision, Estado estado) {
         super(idVehiculo, marca, modelo, cambios, velocidadMaxima, cilindraje, transmision, estado);
-        this.combustible = combustible;
+        combustible = "Diesel";
     }
 
     public String getCombustible() {
         return combustible;
     }
 
-    public void setCombustible(String combustible) {
-        this.combustible = combustible;
-    }
 }
