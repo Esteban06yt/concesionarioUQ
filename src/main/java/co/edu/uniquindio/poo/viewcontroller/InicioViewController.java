bc977c7ad177255caffb1afd3922db7acb755918
Juanda2312 <juand.tapierom@uqvirtual.edu.co>
2024-11-17 17:33:30 -0500

    Actualización Parcial

--- src/main/java/co/edu/uniquindio/poo/viewcontroller/InicioViewController.java
+++ src/main/java/co/edu/uniquindio/poo/viewcontroller/InicioViewController.java
@@ -9,6 +9,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class InicioViewController {
@@ -19,18 +24,41 @@ public class InicioViewController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // fx:id="btnAbrirVentanaCliente"
-    private Button btnAbrirVentanaCliente; // Value injected by FXMLLoader
+    @FXML // fx:id="btnIniciarSesion"
+    private Button btnIniciarSesion; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnAbrirVentanaEmpleado"
-    private Button btnAbrirVentanaEmpleado; // Value injected by FXMLLoader
+    @FXML // fx:id="menuBtnYoSoy"
+    private MenuButton menuBtnYoSoy; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnAbrirVentanaAdmin"
-    private Button btnAbrirVentanaAdmin; // Value injected by FXMLLoader
+    @FXML // fx:id="hyperLinkRecuperarContraseña"
+    private Hyperlink hyperLinkRecuperarContraseña; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCorreoElectronico"
+    private TextField txtCorreoElectronico; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtContrasenia"
+    private PasswordField txtContrasenia; // Value injected by FXMLLoader
 
     @FXML
-    void abrirVentanaCliente(ActionEvent event) {
-        //Al presionar el Botón Cliente se abre la ventana para el Login del cliente
+    private MenuItem administradorMenuItem;
+
+    @FXML
+    private MenuItem empleadoMenuItem;
+
+    @FXML
+    private MenuItem clienteMenuItem;
+
+    @FXML
+    public void initialize() {
+        // Configuración de los ítems del menú
+        administradorMenuItem.setOnAction(this::handleSeleccionarAdministrador);
+        empleadoMenuItem.setOnAction(this::handleSeleccionarEmpleado);
+        clienteMenuItem.setOnAction(this::handleSeleccionarCliente);
+    }
+
+
+    @FXML
+    void iniciarSesion(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/poo/ClienteLoginView.fxml"));
             Parent root = loader.load();
@@ -43,23 +71,28 @@ public class InicioViewController {
             e.printStackTrace();
             System.out.println("Error al cargar la vista: " + e.getMessage());
         }
-    }
 
-    @FXML
-    void abrirVentanaEmpleado(ActionEvent event) {
+
 
     }
 
+
+
     @FXML
-    void abrirVentanaAdmin(ActionEvent event) {
+    void recuperarContraseñaAction(ActionEvent event) {
 
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert btnAbrirVentanaCliente != null : "fx:id=\"btnAbrirVentanaCliente\" was not injected: check your FXML file 'InicioView.fxml'.";
-        assert btnAbrirVentanaEmpleado != null : "fx:id=\"btnAbrirVentanaEmpleado\" was not injected: check your FXML file 'InicioView.fxml'.";
-        assert btnAbrirVentanaAdmin != null : "fx:id=\"btnAbrirVentanaAdmin\" was not injected: check your FXML file 'InicioView.fxml'.";
+        assert btnIniciarSesion != null : "fx:id=\"btnIniciarSesion\" was not injected: check your FXML file 'InicioView.fxml'.";
+        assert menuBtnYoSoy != null : "fx:id=\"menuBtnYoSoy\" was not injected: check your FXML file 'InicioView.fxml'.";
+        assert hyperLinkRecuperarContraseña != null : "fx:id=\"hyperLinkRecuperarContraseña\" was not injected: check your FXML file 'InicioView.fxml'.";
+        assert txtCorreoElectronico != null : "fx:id=\"txtCorreoElectronico\" was not injected: check your FXML file 'InicioView.fxml'.";
+        assert txtContrasenia != null : "fx:id=\"txtContrasenia\" was not injected: check your FXML file 'InicioView.fxml'.";
 
     }
 }
+
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,11 @@ module co.edu.uniquindio.poo {
     requires javafx.fxml;
     requires transitive javafx.graphics;
     requires java.logging;
+    requires javafx.base;
 
     opens co.edu.uniquindio.poo.application to javafx.fxml;
     exports co.edu.uniquindio.poo.application;
-    exports co.edu.uniquindio.poo.controller;
+    exports co.edu.uniquindio.poo.viewcontroller;
     exports co.edu.uniquindio.poo.model;
-    opens co.edu.uniquindio.poo.controller to javafx.fxml;
+    opens co.edu.uniquindio.poo.viewcontroller to javafx.fxml;
 }
